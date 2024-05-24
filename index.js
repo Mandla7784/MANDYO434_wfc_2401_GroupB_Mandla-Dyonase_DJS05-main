65998b7c0296eba9ef04fd22d416ca3433389cec
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-05-24 13:23:54 +0200

    Added files via uploads

---
+++ index.js
@@ -0,0 +1,3 @@
+import { store } from "./src/Store.js";
+
+console.log(store.getState());
--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,20 @@
 //defining action types
+const myTypes = {
+  ADD: "ADD",
+  MINUS: "MINUS",
+  RESET: "RESET",
+};
 
-const ADD = "ADD";
-const MINUS = "MINUS";
+const { ADD, MINUS, RESET } = myTypes;
+//action creater...
+export const incrementingState = () => ({
+  type: ADD,
+});
+
+export const decrementingSate = () => ({
+  type: MINUS,
+});
+
+export const resetingSate = () => ({
+  type: RESET,
+});
